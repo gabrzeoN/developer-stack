ff5ec3e9505878e9946491cd08d32d42e6ea3a51
Gabriel Cari <g.cari@hotmail.com>
2022-07-13 22:31:53 -0300

    Fix-send-answer: validating if question exists.

--- src/services/answerService.ts
+++ src/services/answerService.ts
@@ -1,7 +1,11 @@
 import * as answerRepository from "./../repositories/answerRepository.js";
+import * as questionRepository from "./../repositories/questionRepository.js";
 
 export async function create(questionId: number, answer: string) {
-//TODO: if(!question)
+    const question = await questionRepository.getById(questionId);
+    if(!question){
+        throw {type: "notFound", message: "Question does not exist!"}; 
+    }
 
     await answerRepository.create(questionId, answer);
     return;
